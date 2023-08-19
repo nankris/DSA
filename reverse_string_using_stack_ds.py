3ef3d26847ae8e45b5b08f03017340c5e15ad10d
nankris <hari12.nhk@gmail.com>
2023-08-19 09:54:09 +0530

    Create reverse_string_using_stack_ds.py

---
+++ reverse_string_using_stack_ds.py
@@ -0,0 +1,43 @@
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+
+    def print_stack(self):
+        for i in range(len(self.stack_list)-1, -1, -1):
+            print(self.stack_list[i])
+
+    def is_empty(self):
+        return len(self.stack_list) == 0
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list[-1]
+
+    def size(self):
+        return len(self.stack_list)
+
+    def push(self, value):
+        self.stack_list.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list.pop()
+
+# below code is what i have written
+def reverse_string(my_string):
+    stack1 = Stack()
+    for i in my_string:
+        stack1.push(i)
+    new_str = ''
+    while stack1.size() != 0:
+        new_str += stack1.pop()
+    return new_str
+
+
+my_string = 'hello'
+
+print ( reverse_string(my_string) )
